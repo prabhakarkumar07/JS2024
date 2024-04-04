97cac8e0e591522355c0b89d755c309eaeb57012
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 15:13:29 +0000

    data type

---
+++ 01_Js/datatype.js
@@ -0,0 +1,22 @@
+"use strict";
+//treat all js code as newer version of javascript
+
+//  alert(3*3); we are using node js not browser
+// console.log(3+5);
+// console.log("Prabhakar")//code readiblity should be high
+
+// let name="prabhakr"//string
+// let age=18//number
+// let isLogedIn=false//bool
+//number safe limit
+//bigInt
+//string=>""
+//bool=>true or fasle
+//null standalone value rep of empty value let name=null;
+//undefuned=>let name;
+//symbol => to find uniqueness in components
+
+
+//object
+
+console.log(typeof(undefined));

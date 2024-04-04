ba04c03f58e7028225c65b1b0bdf3f11fab8198b
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 15:26:34 +0000

    conver of datatype

---
+++ 01_Js/conversionOpration.js
@@ -0,0 +1,27 @@
+// let score="prabhakar";
+
+// console.log(typeof(score));
+// let valueInNumber=Number(score);
+// console.log(valueInNumber);
+
+// "22"=>22
+//if convert in number
+//"545fs"=>Nan
+
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof (booleanIsLoggedIn));
+//true=1
+//false=0
+//" "=false
+//"prabhakr"=>true
+
+///string
+
+let someNumber=555
+let stringNumber=String(someNumber)
+console.log(typeof(stringNumber));
+

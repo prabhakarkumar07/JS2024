a330e12b0ac4693af9b51f0f09386b0f25669309
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 17:55:37 +0000

    premitive(stack)andnon(heap)

--- 01_Js/heapandstack.js
+++ 01_Js/heapandstack.js
@@ -1,1 +1,12 @@
-console.log("Heap and stack");
+//stack(premitive) and heap(non-primitive)
+let myName="Prabhakar.com"
+let anotherName=myName;
+anotherName="chai aur code"
+console.log(myName);
+console.log(anotherName);
+
+let user1={
+    email:"user@gmail.com",
+    name:"user1",
+}
+console.log(user1.email);

905795cf0f2c6619dee02378bad8aad884d6e566
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 16:12:50 +0000

    datatypeSummmry

---
+++ 01_Js/datatypeSummry.js
@@ -0,0 +1,23 @@
+ //the way data is storing in memory
+
+//premitive datatype
+//7 types=>string,Number,boolean,null,undefined,Symbol,BigInt
+
+//js is dynamically type language
+
+ 
+//non-premitive datatype || Reference type
+ //type=>array[],Objects{ key:value},function functionname{}
+
+const hero=["shaktiman","nagraj","doga"];
+let myObj={
+    name:"Prabakar",
+    age:22,
+}
+console.log(myObj);
+
+function myFunction()
+{
+console.log("Prabhakar");
+}
+myFunction();

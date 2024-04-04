c50692f3c1cf94f742cf0b41ba0682d67f648580
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 18:42:22 +0000

    math functions

---
+++ 01_Js/numsandmath.js
@@ -0,0 +1,25 @@
+// const score=400.45265
+// console.log(score);
+// const balance=new Number(score);
+
+// console.log( balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//////////+++++++++++Math++++++++++++///////////
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(1,5,8,3));
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);

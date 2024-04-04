925b9bc6840d89ca67995f8e9d8a841c722a73c5
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-04 18:18:01 +0000

    string functions

---
+++ 01_Js/string.js
@@ -0,0 +1,33 @@
+// const name='prabhakar';
+// const repoCount=50;
+// console.log(`Hello my name is ${name.toUpperCase()} and my repoCount ${repoCount}`);
+const gameName=new String("Prabhakar");
+// console.log( gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('r'));
+// const newstring=gameName.substring(0,4)
+// console.log(newstring);
+// const sliceString=gameName.slice(1,4);
+// //console.log(sliceString);
+// const newStringOne="  Prabhakar  ";
+// console.log(newStringOne.trim());
+
+const url="https://prabhakar%kumar-portfolio.vercel.app";
+console.log(url.replace('%','-'));
+
+console.log(url.includes('raj'));
+console.log(gameName.split( ''));
+console.log(gameName.trim(" "));
+console.log(gameName.bold());
+
+
+
+
+
+
+
+
+
+

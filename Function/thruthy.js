4e073b00d3a90677358721df6a2ad527accffb08
Prabhakar Kumar <76665150+prabhakarkumar07@users.noreply.github.com>
2024-04-06 03:44:10 +0000

    Nulish Coalecen

---
+++ Function/thruthy.js
@@ -0,0 +1,18 @@
+
+//Nullish coalecing opre (??)
+let val1;
+
+//val1=5??10;=>5
+//val1=null??10;=>10
+//val1=undefined??10;==>10
+//val1=null??10??20;//will return first value
+//console.log(val1);
+
+
+///Ternairy oprator
+
+//condition?true:false
+
+// const iceTeaPrice=100;
+// iceTeaPrice<=80?console.log("less then 80"):console.log("more then 80");
+
